a61b325c046a2643bcae35c9d4dd02358b6c771e
JHX549 <jiang_549@outlook.com>
2024-04-02 11:26:47 -0700

    Revert "Scroll feature"
    This reverts commit c23baed4830ee34c22d168c14e5f895d47df776c.

--- main.js
+++ main.js
@@ -1,32 +1,15 @@
-// const items = document.querySelectorAll('.appear');
-
-// const active = function (entries) {
-//   entries.forEach(entry => {
-//     if (entry.isIntersecting) {
-//       entry.target.classList.add('inview');
-//     } else {
-//       entry.target.classList.remove('inview');
-//     }
-//   });
-// };
-// const io2 = new IntersectionObserver(active);
-// for (let i = 0; i < items.length; i++) {if (window.CP.shouldStopExecution(0)) break;
-//   io2.observe(items[i]);
-// }window.CP.exitedLoop(0);
-const items = document.querySelectorAll('.appear');
-
-const active = function (entries) {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add('inview');
+$(window).on("scroll", function () {
+    if (window.scrollY > window.outerHeight) {
+      $("#futurebtm").addClass("active");
+      setTimeout(function() {
+        var theta = ($(window).scrollTop() - (window.outerHeight + (window.outerHeight/2))) / 500;
+        $('#futurebtm').css({ transform: 'rotate(' + theta + 'rad)' });
+      })
     } else {
-      entry.target.classList.remove('inview');
+      $("#futurebtm").removeClass("active");
     }
   });
-};
-
-const io2 = new IntersectionObserver(active);
-
-items.forEach(item => {
-  io2.observe(item);
-});
+  
+  $('#futurebtm').on('click',function() {
+    $("html, body").animate({ scrollTop: 0 }, 300000000);
+  })
